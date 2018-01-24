efa3657a4277ea7bd2b62382091395f57f1558f1
MOUNIKA <mounajakkani@gmail.com>
2018-01-24 12:47:04 -0800

    modified server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(function(req, res, next)){
+app.use(function (req, res, next){
   if(req.headers['x-forwarded-proto'] === 'http'){
     next();
   }else {
@@ -17,5 +17,5 @@ app.use(function(req, res, next)){
 app.use(express.static('public'));
 
 app.listen(PORT, function() {
-  console.log('Express server is up on port 3000' + PORT);
+  console.log('Express server is up on port ' + PORT);
 });
