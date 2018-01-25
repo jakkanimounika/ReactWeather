9302751ced388d153213f84526962cca7ecb7d05
MOUNIKA <mounajakkani@gmail.com>
2018-01-24 18:23:23 -0800

    added div

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,10 @@ var React = require('react');
 
 var About = (props) => {
   return(
-    <h1>About component</h1>
-    <p>Welcome to About page</p>
+    <div>
+      <h1>About component</h1>
+      <p>Welcome to About page</p>
+    </div>
   );
 };
 
--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -2,9 +2,11 @@ var React = require('react');
 
   var Examples = (props) => {
     return(
-      <h1>Examples Component!</h1>
-      <p>Welcome to Examples page</p>
-    );
+      <div>
+        <h1>Examples Component!</h1>
+        <p>Welcome to Examples page</p>
+      </div>
+);    
   };
 
 module.exports = Examples;
