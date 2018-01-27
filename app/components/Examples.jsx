85dfa72e6472ffa8819b7416b299247b5ed36fd8
MOUNIKA <mounajakkani@gmail.com>
2018-01-27 10:30:36 -0800

    add nav search support

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,21 +1,21 @@
 var React = require('react');
-var {Link} = require('react-router')
+var {Link} = require('react-router');
 
-  var Examples = (props) => {
-    return(
-      <div>
-        <h1 className="text-center page-title">Examples</h1>
-        <p>Here are a few example locations to try out:</p>
-        <ol>
-          <li>
-            <Link to='/?location=Santa clara'>Santa clara, CA</Link>
-          </li>
-          <li>
-            <Link to='/?Location=Rio'>Rio, Brazil</Link>
-          </li>
-        </ol>
-      </div>
-);
-  };
+var Examples = (props) => {
+  return (
+    <div>
+      <h1 className="text-center page-title">Examples</h1>
+      <p>Here are a few example locations to try out:</p>
+      <ol>
+        <li>
+          <Link to='/?location=Philadelphia'>Philadelphia, PA</Link>
+        </li>
+        <li>
+          <Link to='/?location=Rio'>Rio, Brazil</Link>
+        </li>
+      </ol>
+    </div>
+  )
+};
 
 module.exports = Examples;
--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -15,11 +15,13 @@ var Weather = React.createClass({
 
     this.setState({
       isLoading: true,
-      errorMessage: undefined
+      errorMessage: undefined,
+      location: undefined,
+      temp: undefined
     });
 
     openWeatherMap.getTemp(location).then(function (temp) {
-      debugger;
+
       that.setState({
         location: location,
         temp: temp,
@@ -30,8 +32,26 @@ var Weather = React.createClass({
         isLoading: false,
         errorMessage: e.message
       });
-        alert(errorMessage);
+
      });
+  },
+  componentDidMount: function () {
+    var location = this.props.location.query.location;
+
+    if(location && location.length > 0) {
+      this.handleSearch(location);
+      window.location.hash = '#/';
+    }
+  },
+
+  componentWillReceiveProps: function (newProps) {
+    var location = newProps.location.query.location;
+
+    if(location && location.length > 0) {
+      this.handleSearch(location);
+      window.location.hash = '#/';
+    }
+
   },
   render: function () {
     var {isLoading, temp, location, errorMessage} = this.state;
